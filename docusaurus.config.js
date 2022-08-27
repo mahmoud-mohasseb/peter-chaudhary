7d6d90d67874c9d8942bbc7e41fa3eeaf73a5ecf
Mac X <ghareb4@gmail.com>
2022-08-27 17:43:30 +0300

    change to verify agolio

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,7 +127,31 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
-      
+      algolia: {
+        // The application ID provided by Algolia
+        appId: process.env.ALGOLIA_APPID,
+  
+        // Public API key: it is safe to commit it
+        apiKey:process.env.ALGOLIA_API_KEY ,
+  
+        indexName: 'peter',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Algolia search parameters
+        searchParameters: {
+          
+        },
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
       
     }),
 };
