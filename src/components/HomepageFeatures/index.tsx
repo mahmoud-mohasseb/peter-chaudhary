b245bb68e33cf340e16c636f7025b9f2367be6bc
Mac X <ghareb4@gmail.com>
2022-08-29 15:24:24 +0300

    changes done version img

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -21,7 +21,7 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: "Focus on What Matters",
-    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    Svg: require("@site/static/img/undraw_business_shop_re_ruf4.svg").default,
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -31,7 +31,7 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: "Powered by React",
-    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    Svg: require("@site/static/img/undraw_business_chat_re_gg4h.svg").default,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
