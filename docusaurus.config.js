3228987bcc3cece6929d213a5eb442ec6c8c9ed9
Mac X <ghareb4@gmail.com>
2022-09-04 14:34:42 +0300

    netlify cms broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ const config = {
   tagline: "Peter documentation and blogs",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
-  onBrokenLinks: "ignore",
-  onBrokenMarkdownLinks: "ignore",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
   // favicon: 'img/favicon.ico',
   favicon:
     "https://cdn.dribbble.com/users/60166/screenshots/17217488/media/02728f99e977931182d683b6792a17b1.jpg?compress=1&resize=400x300",
